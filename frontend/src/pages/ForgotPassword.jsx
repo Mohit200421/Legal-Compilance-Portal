234c8a1a9d00e1719a876b962f0bad007904f488
Mohit Badgujar <144667403+Mohit200421@users.noreply.github.com>
2026-01-21 14:08:26 +0530

    Fix ForgotPassword css case

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../api/axios";
+import "./ForgotPassword.css";
 
 
 export default function ForgotPassword() {

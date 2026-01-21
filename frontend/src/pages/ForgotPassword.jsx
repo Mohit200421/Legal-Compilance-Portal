baa183f63f9a029252247a4c480f5f4687f6b406
Mohit Badgujar <144667403+Mohit200421@users.noreply.github.com>
2026-01-21 13:21:14 +0530

    Remove  forgot.css error

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../api/axios";
-import "./forgotpassword.css";
+import "./ForgotPassword.css";
 
 export default function ForgotPassword() {
   const navigate = useNavigate();

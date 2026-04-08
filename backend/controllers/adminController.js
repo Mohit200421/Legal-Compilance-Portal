4944a42e3792ee64fbadc112b46de49ea65ee79d
Mohit Badgujar <144667403+Mohit200421@users.noreply.github.com>
2026-04-08 15:03:45 +0530

    temp

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -6,6 +6,7 @@ const Lawyer = require("../models/Lawyer");
 const Job = require("../models/Job");
 const News = require("../models/News");
 const { sendEmail } = require("../utils/emailService");
+const bcrypt = require("bcrypt");
 
 /* ================= USERS =================== */
 
